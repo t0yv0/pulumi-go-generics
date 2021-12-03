32c2e50eadda5d3a41b0aa6e1aa4ba0cfb6c8c12
Anton Tayanovskyy <anton@pulumi.com>
2021-12-02 22:53:09 -0800

    Fix map todo

--- outputs/map.go
+++ outputs/map.go
@@ -1,15 +1,27 @@
 package outputs
 
-import (
-	p "github.com/t0yv0/pulumi-go-generics/promises"
-)
-
 type Map[K comparable, V any] map[K]Output[V]
 
-func (m Map[K,V]) Context() *Context {
-	panic("TODO")
+func (m Map[K,V]) ToOutput() Output[map[K]V] {
+	inner := map[K]Output[V](m)
+
+	var ctx *Context
+	for _, v := range inner {
+		ctx = context(v)
+		break
+	}
+
+	if ctx == nil {
+		panic("Map should have at least 1 element")
+	}
+
+	return Apply(ctx, func (t *T) map[K]V {
+		result := make(map[K]V)
+		for k, v := range inner {
+			result[k] = Eval(t, v)
+		}
+		return result
+	})
 }
 
-func (m Map[K,V]) toPromise() *p.Promise[*outputResult[map[K]V]] {
-	panic("TODO")
-}
+var _ Output[map[string]string] = Map[string,string]{}
