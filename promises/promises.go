e16bef7566c94a10b8e85d36a8023c39c802280e
Anton Tayanovskyy <anton@pulumi.com>
2021-12-02 23:50:48 -0800

    Clean promises

--- promises/promises.go
+++ promises/promises.go
@@ -1,9 +1,5 @@
 package promises
 
-import (
-	"fmt"
-)
-
 const (
 	PendingState = iota
 	ResolvedState
@@ -76,7 +72,6 @@ func NewPromise[T any](observer Observer) (*Promise[T], func(T), func(error)) {
 	reject := func(err error) {
 		f.err = err
 		f.state = RejectedState
-		fmt.Printf("reject error=%v\n", err)
 		close(complete) // possible "panic: close of closed channel"
 		observer.Rejected(err)
 	}
@@ -91,14 +86,13 @@ func (p *Promise[T]) Await() (T, error) {
 
 func MapErr[T any, U any](f func(T) (U, error)) func (*Promise[T]) *Promise[U] {
 	return func(p *Promise[T]) *Promise[U] {
-		var res *Promise[U]
 		switch p.state {
 		case ResolvedState:
 			v, err := f(p.value)
 			if err != nil {
-				res = Rejected[U](p.observer, err)
+				return Rejected[U](p.observer, err)
 			} else {
-				res = Resolved(p.observer, v)
+				return Resolved(p.observer, v)
 			}
 		case RejectedState:
 			return Rejected[U](p.observer, p.err)
@@ -108,11 +102,13 @@ func MapErr[T any, U any](f func(T) (U, error)) func (*Promise[T]) *Promise[U] {
 				v, err := res.Await()
 				if err != nil {
 					reject(err)
+				} else {
+					resolve(v)
 				}
-				resolve(v)
 			}()
+			return res
 		}
-		return res
+		panic("Incomplete pattern match")
 	}
 }
 
@@ -120,85 +116,4 @@ func Map[T any, U any](f func(T) U) func (*Promise[T]) *Promise[U] {
 	return MapErr(func (x T) (U, error) {
 		return f(x), nil
 	})
-}
-
-func All[T any](observer Observer, promises []*Promise[T]) *Promise[[]T] {
-	if len(promises) == 0 {
-		return Resolved(observer, []T{})
-	}
-
-	allPending := true
-	for _, p := range promises {
-		if p.state != PendingState {
-			allPending = false
-		}
-	}
-
-	if allPending {
-		values := []T{}
-		for _, p := range promises {
-			if p.state == RejectedState {
-				return Rejected[[]T](observer, p.err)
-			}
-			values = append(values, p.value)
-		}
-		return Resolved(observer, values)
-	}
-
-	result, resolve, reject := NewPromise[[]T](observer)
-
-	completed := make(chan int)
-
-	observe := func(i int) {
-		<- promises[i].complete
-		completed <- i
-	}
-
-	for i := range promises {
-		go observe(i)
-	}
-
-	go func() {
-		done := 0
-		results := make([]T, len(promises))
-		for result.state == PendingState {
-			i := <-completed
-			if promises[i].state == RejectedState {
-				reject(promises[i].err)
-			} else {
-				results[i] = promises[i].value
-				done++
-				if done == len(promises) {
-					resolve(results)
-				}
-			}
-		}
-	}()
-
-	return result
-}
-
-func Join[T any](pp *Promise[*Promise[T]]) *Promise[T] {
-	switch pp.state {
-	case RejectedState:
-		return Rejected[T](pp.observer, pp.err)
-	case ResolvedState:
-		return pp.value
-	default:
-		result, resolve, reject := NewPromise[T](pp.observer)
-		go func() {
-			f, err := pp.Await()
-			if err != nil {
-				reject(err)
-				return
-			}
-			v, err := f.Await()
-			if err != nil {
-				reject(err)
-				return
-			}
-			resolve(v)
-		}()
-		return result
-	}
 }
